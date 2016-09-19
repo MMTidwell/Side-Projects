b3f11f7953c39d11f6ba92c127894d2e7494ef42
Mittsy Tidwell <mtidwell506@gmail.com>
2016-09-19 18:38:42 -0500

    everything is workinggit add -A WOOT

--- public/magic_eight/magic_eight.js
+++ public/magic_eight/magic_eight.js
@@ -24,74 +24,14 @@
 
 	// ====================== RANDOM NUM GEN =============================
 	// ====================== ANSWER =============================
-	function randomNumber() { 
-		var num = Math.floor(Math.random() * 20) + 1;
-		// console.log("your number is: " + num)
+	var answers = ['Maybe', 'Sure', 'Certainly not', 'I hope for your sake', 'Nope!', 'There is a possibility', 'Quite likely', 'I hope not!', 'NEVER!!', 'Ahaha! Really?!?', 'Pfft', 'Hell YES!', 'Hell to the NO', 'Hell YES!!', ' Who cares?', 'Yes', 'It is certain', 'It is decidedlu so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
 
-		setTimeout(function() {
-			switch (num) {
-				case 1:
-					$("#answer").html("It is certain");
-					break;
-				case 2:
-					$("#answer").html("It is decidedly so");
-					break;
-				case 3:
-					$("#answer").html("Without a doubt");
-					break;
-				case 4:
-					$("#answer").html("Yes, definitely");
-					break;
-				case 5:
-					$("#answer").html("You may rely on it");
-					break;
-				case 6:
-					$("#answer").html("As I see it, yes");
-					break;
-				case 7:
-					$("#answer").html("Most likely");
-					break;
-				case 8:
-					$("#answer").html("Outlook good");
-					break;
-				case 9:
-					$("#answer").html("Yes");
-					break;
-				case 10:
-					$("#answer").html("Signs point to yes");
-					break;
-				case 11:
-					$("#answer").html("Reply hazy try again");
-					break;
-				case 12:
-					$("#answer").html("Ask again later");
-					break;
-				case 13:
-					$("#answer").html("Better not tell you now");
-					break;
-				case 14:
-					$("#answer").html("Cannot predict now");
-					break;
-				case 15:
-					$("#answer").html("Concentrate and ask again");
-					break;
-				case 16:
-					$("#answer").html("Don't count on it");
-					break;
-				case 17:
-					$("#answer").html("My reply is no");
-					break;
-				case 18:
-					$("#answer").html("My sources say no");
-					break;
-				case 19:
-					$("#answer").html("Outlook not so good");
-					break;
-				case 20:
-					$("#answer").html("Very doubtful");
-					break;
-			}
-		}, 700);
+	function randomNumber() { 
+		var random = Math.floor((Math.random() * answers.length - 1) + 0);
+		var answer = answers[random];
+			setTimeout(function() {
+				$(".triangle").html(answer);
+			}, 700);
 	}
 	
 play();
